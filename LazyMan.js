1f21fc0270d96fb6e3d80e567cabd5da7c09abdd
albireo145 <34166466+albireo145@users.noreply.github.com>
2020-01-20 18:21:35 +0800

    Update LazyMan.js

--- LazyMan.js
+++ LazyMan.js
@@ -3,72 +3,37 @@ function LazyMan(name) {
     name: name,
     id: null,
     queue: [],
-    delay: (fn, t) => {
-      return new Promise(resolve => setTimeout(() => {
-        fn()
-        resolve()
-      }, t))
-    },
     init: function() {
       var _this = obj;
-      if (_this.id) {
-        clearTimeout(_this.id);
-      }
       _this.queue.push({ func: () => console.log(`hi ${name}`), t: 0 });
-      var id = setTimeout(function() {
-        var temp = _this.delay(function(){}, 0)
-        for(let item of _this.queue) {
-          temp = temp.then(() => _this.delay(item.func, item.t))
-        }
+      setTimeout(() => {
+        _this.next()
       }, 0)
-      _this.id = id;
     },
     eat: function(shit) {
       var _this = obj;
-      if (_this.id) {
-        clearTimeout(_this.id);
-      }
       _this.queue.push({ func: () => console.log(`eat ${shit}`), t: 0 });
-      var id = setTimeout(function() {
-        var temp = _this.delay(function(){}, 0)
-        for(let item of _this.queue) {
-          temp = temp.then(() => _this.delay(item.func, item.t))
-        }
-      }, 0)
-      _this.id = id;
       return _this;
     },
     sleep: function(second) {
       var _this = obj;
-      if (_this.id) {
-        clearTimeout(_this.id);
-      }
       _this.queue.push({ func: () => console.log(`sleep ${second}s...`), t: 0 });
       _this.queue.push({ func: () => console.log(`awake!`), t: second * 1000 });
-      var id = setTimeout(function() {
-        var temp = _this.delay(function(){}, 0)
-        for(let item of _this.queue) {
-          temp = temp.then(() => _this.delay(item.func, item.t))
-        }
-      }, 0)
-      _this.id = id;
       return _this;
     },
     sleepFirst: function(second) {
       var _this = obj;
-      if (_this.id) {
-        clearTimeout(_this.id);
-      }
       _this.queue.unshift({ func: () => console.log(`awake!`), t: second * 1000 });
       _this.queue.unshift({ func: () => console.log(`Sleep First ${second}s...`), t: 0 });
-      var id = setTimeout(function() {
-        var temp = _this.delay(function(){}, 0)
-        for(let item of _this.queue) {
-          temp = temp.then(() => _this.delay(item.func, item.t))
-        }
-      }, 0)
-      _this.id = id;
       return _this;
+    },
+    next: function() {
+      var _this = obj
+      var cur = _this.queue.shift()
+      cur && cur.func && setTimeout(() => {
+        cur.func()
+        _this.next()
+      }, cur.t);
     }
   };
   obj.init();
