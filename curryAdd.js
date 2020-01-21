73c905790e47d8a3892ec44eca52addca2bce11c
albireo145 <34166466+albireo145@users.noreply.github.com>
2020-01-21 11:26:58 +0800

    Create curryAdd.js

---
+++ curryAdd.js
@@ -0,0 +1,16 @@
+function add() {
+  var args = []
+  var func = function() {
+    args = [...args, Array.from(arguments)]
+    return func
+  }
+  func.valueOf = function() {
+    return args.reduce((sum, cur) => sum + Number(cur), 0)
+  }
+  func.toString = function() {
+    return args.reduce((sum, cur) => sum + Number(cur), 0)
+  }
+  return func
+}
+
+console.log(add(1, 2)(3)(4) + 0)
